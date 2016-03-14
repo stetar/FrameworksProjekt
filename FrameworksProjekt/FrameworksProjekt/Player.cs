9e236d5efa9a9c92fb71867455d8648719026edd
UserNameIsAlreadyTaken1 <holsteinkruse@gmail.com>
2016-03-14 12:11:13 +0100

    Minor changes

---
+++ FrameworksProjekt/FrameworksProjekt/Player.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace FrameworksProjekt
+{
+    enum Direction
+    {
+        Up, 
+        Down, 
+        Left, 
+        Right
+    }
+
+    class Player : Component, ILoadable, IUpdateable
+    {
+        private float speed = 100;
+        KeyboardState ks;
+        public Animator animator;
+        private Direction direction;
+        //private IStrategy strategy;
+
+        public Player(GameObject gameObject) : base(gameObject)
+        {
+            direction = Direction.Down;
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            
+        }
+
+        public void LoadContent(ContentManager content)
+        {
+            
+        }
+
+        public void Update()
+        {
+            
+        }
+    }
+}
