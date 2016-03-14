ae95bbebc9f8726831a94bc005d7f021b681e4c4
UserNameIsAlreadyTaken1 <holsteinkruse@gmail.com>
2016-03-14 10:44:43 +0100

    Added gameobject

---
+++ FrameworksProjekt/FrameworksProjekt/GameObject.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+
+namespace FrameworksProjekt
+{
+    class GameObject
+    {
+
+        private List<Component> components;
+
+        public GameObject()
+        {
+
+        }
+
+        public void LoadContent(ContentManager content)
+        {
+            foreach(Component c in components)
+            {
+                if(c is ILoadable)
+                {
+                    (c as ILoadable).LoadContent(content);
+                }
+            }
+        }
+
+        public void Update()
+        {
+            foreach(Component c in components)
+            {
+                if(c is IUpdateable)
+                {
+                    (c as IUpdateable).Update();
+                }
+            }
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            foreach(Component c in components)
+            {
+                if(c is IDrawable)
+                {
+                    (c as IDrawable).Draw(spriteBatch);
+                }
+            }
+        }
+
+        public void AddComponent(Component component)
+        {
+            components.Add(component);
+        }
+
+        public Component GetComponent(string component)
+        {
+            Component toReturn = null;
+
+            foreach(Component c in components)
+            {
+                if(c.GetType().Name == component)
+                {
+                    toReturn = c;
+                }
+            }
+
+            return toReturn;
+        }
+
+        public void DeleteComponent(string component)
+        {
+            foreach(Component c in components.ToList())
+            {
+                if(c.GetType().Name == component)
+                {
+                    components.Remove(c);
+                }
+            }
+        }
+    }
+}
