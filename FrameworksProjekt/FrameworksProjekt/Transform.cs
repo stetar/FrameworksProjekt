3a5ee4d4cfa59541a4c6cba5789527b8eded064c
UserNameIsAlreadyTaken1 <holsteinkruse@gmail.com>
2016-03-14 11:13:43 +0100

    Some changes

---
+++ FrameworksProjekt/FrameworksProjekt/Transform.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FrameworksProjekt
+{
+    class Transform : Component
+    {
+        public Vector2 Position { get; set; }
+
+        public Transform(GameObject gameObject, Vector2 position) : base(gameObject)
+        {
+            this.Position = position;
+        }
+
+        public void Translate(Vector2 translation)
+        {
+            this.Position += translation;
+        }
+    }
+}
